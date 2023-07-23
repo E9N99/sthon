070647dcbaa4e78c33e2216e66cd2f3d71b4684a
BiLaL <97440505+E9N99@users.noreply.github.com>
2023-07-24 00:38:43 +0300

    Add files via upload

---
+++ HuRe/__main__.py
@@ -0,0 +1,90 @@
+import sys
+import HuRe
+from HuRe import BOTLOG_CHATID, HEROKU_APP, PM_LOGGER_GROUP_ID
+from .Config import Config
+from .core.logger import logging
+from .core.session import l313l
+from .utils import (
+    add_bot_to_logger_group,
+    install_externalrepo,
+    ipchange,
+    load_plugins,
+    setup_bot,
+    mybot,
+    startupmessage,
+    verifyLoggerGroup,
+    saves,
+)
+
+LOGS = logging.getLogger("HuRe")
+
+print(HuRe.__copyright__)
+print("Licensed under the terms of the " + HuRe.__license__)
+
+cmdhr = Config.COMMAND_HAND_LER
+
+try:
+    LOGS.info("جارِ بدء بوت الجوكر ✓")
+    l313l.loop.run_until_complete(setup_bot())
+    LOGS.info("تم اكتمال تنصيب البوت ✓")
+except Exception as e:
+    LOGS.error(f"{str(e)}")
+    sys.exit()
+
+try:
+    LOGS.info("يتم تفعيل وضع الانلاين")
+    l313l.loop.run_until_complete(mybot())
+    LOGS.info("تم تفعيل وضع الانلاين بنجاح ✓")
+except Exception as jep:
+    LOGS.error(f"- {jep}")
+    sys.exit()    
+
+class CatCheck:
+    def __init__(self):
+        self.sucess = True
+
+
+Catcheck = CatCheck()
+
+
+async def startup_process():
+    check = await ipchange()
+    if check is not None:
+        Catcheck.sucess = False
+        return
+    await verifyLoggerGroup()
+    await load_plugins("plugins")
+    await load_plugins("assistant")
+    print("➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖")
+    print("᯽︙بـوت الجوكر يعـمل بـنجاح ")
+    print(
+        f"تم تشغيل الانلاين تلقائياً ارسل {cmdhr}الاوامر لـرؤيـة اوامر السورس\
+        \nللمسـاعدة تواصـل  https://t.me/JepthonSupport"
+    )
+    print("➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖")
+    await verifyLoggerGroup()
+    await saves()
+    await add_bot_to_logger_group(BOTLOG_CHATID)
+    if PM_LOGGER_GROUP_ID != -100:
+        await add_bot_to_logger_group(PM_LOGGER_GROUP_ID)
+    await startupmessage()
+    Catcheck.sucess = True
+    return
+
+async def externalrepo():
+    if Config.VCMODE:
+        await install_externalrepo("https://github.com/jepthoniq/JepVc", "jepvc", "jepthonvc")
+
+l313l.loop.run_until_complete(externalrepo())
+l313l.loop.run_until_complete(startup_process())
+
+if len(sys.argv) not in (1, 3, 4):
+    l313l.disconnect()
+elif not Catcheck.sucess:
+    if HEROKU_APP is not None:
+        HEROKU_APP.restart()
+else:
+    try:
+        l313l.run_until_disconnected()
+    except ConnectionError:
+        pass
