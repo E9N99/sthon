825a37a9eb313fbc12234837ddedb67125a90f2d
BiLaL <97440505+E9N99@users.noreply.github.com>
2023-07-24 01:11:56 +0300

    Update تحديث.py

--- HuRe/plugins/تحديث.py
+++ HuRe/plugins/تحديث.py
@@ -326,7 +326,7 @@ async def Hussein(event):
             f"I guess you are on selfhost. For self host you need to use `{cmdhd}update now`",
         )
     event = await edit_or_reply(event, "**᯽︙ جارِ تحديث ريبو التنصيب لسورس الجوكر **")
-    off_repo = "https://github.com/jepthoniq/lMl10l"
+    off_repo = "https://github.com/E9N99/Repo"
     os.chdir("/app")
     try:
         txt = (
