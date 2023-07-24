0ddc4469d75123da5bf27e4d3dbe1bd0608bd2e4
BiLaL <97440505+E9N99@users.noreply.github.com>
2023-07-24 16:53:39 +0300

    Update __init__.py

--- HuRe/__init__.py
+++ HuRe/__init__.py
@@ -8,7 +8,7 @@ from .core.session import l313l
 from .sql_helper.globals import addgvar, delgvar, gvarstatus
 
 __version__ = "3.1.3"
-__license__ = كـتابة وتـعديل فريـق سيدثون"
+__license__ = "كـتابة وتـعديل فريـق سيدثون"
 __author__ = "سيدثون <https://T.ME/E9N99>"
 __copyright__ = "BiLaL TEAM (C) 2021 - 2023  " + __author__
 
