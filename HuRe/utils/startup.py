88932fa25cfd539a7bc024084b264073a2ef8814
BiLaL <97440505+E9N99@users.noreply.github.com>
2023-08-04 02:05:57 +0300

    Update startup.py

--- HuRe/utils/startup.py
+++ HuRe/utils/startup.py
@@ -28,7 +28,6 @@ from ..sql_helper.globals import addgvar, delgvar, gvarstatus
 from .pluginmanager import load_module
 from .tools import create_supergroup
 LOGS = logging.getLogger("HuRe")
- logging.getLogger('telethon').setLevel(logging.WARNING)
 ##BiLaL hands here
 cmdhr = Config.COMMAND_HAND_LER
 bot = l313l
