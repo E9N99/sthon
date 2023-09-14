b99b163024ce64788a43f3bcc2c0722b8a5046a0
BiLaL <97440505+E9N99@users.noreply.github.com>
2023-09-14 23:07:49 +0300

    Update التكرار.py

--- HuRe/plugins/التكرار.py
+++ HuRe/plugins/التكرار.py
@@ -269,3 +269,153 @@ async def stopspamrz(event):
         delgvar("spamwork")
         return await edit_delete(event, "**⌔∮ تم بنجاح ايقاف التكرار **")
     return await edit_delete(event, "**⌔∮ عذرا لم يتم تفعيل التكرار بالاصل**")
+    #تم اضافة الاوامر ادناه بمساعدة من فريق الجوكر
+
+async def aljoker_nshr(l313l, sleeptimet, chat, message, seconds):
+    global yaAli
+    yaAli = True
+    while yaAli:
+        if message.media:
+            sent_message = await l313l.send_file(chat, message.media, caption=message.text)
+        else:
+            sent_message = await l313l.send_message(chat, message.text)
+        await asyncio.sleep(sleeptimet)
+@l313l.ar_cmd(pattern="نشر")
+async def Hussein(event):
+    await event.delete()
+    parameters = re.split(r'\s+', event.text.strip(), maxsplit=2)
+    if len(parameters) != 3:
+        return await edit_delete(
+            event, "⌔∮ يجب استخدام كتابة صحيحة الرجاء التاكد من الامر اولا ⚠️"
+        )
+    seconds = int(parameters[1])
+    chat_usernames = parameters[2].split()
+    l313l = event.client
+    global yaAli
+    yaAli = True
+    message = await event.get_reply_message()
+    for chat_username in chat_usernames:
+        try:
+            chat = await l313l.get_entity(chat_username)
+            await aljoker_nshr(l313l, seconds, chat.id, message, seconds)  # تمرير قيمة seconds هنا لكل مجموعة
+        except Exception as e:
+            await edit_delete(
+                event, f"⌔∮ لا يمكن العثور على المجموعة أو الدردشة {chat_username}: {str(e)}"
+            )
+        await asyncio.sleep(1)
+    
+async def aljoker_allnshr(l313l, sleeptimet, message):
+    global yaAli
+    yaAli = True
+    aljoker_chats = await l313l.get_dialogs()
+    while yaAli:
+        for chat in aljoker_chats:
+            if chat.is_group:
+                try:
+                    if message.media:
+                        await l313l.send_file(chat.id, message.media, caption=message.text)
+                    else:
+                        await l313l.send_message(chat.id, message.text)
+                except Exception as e:
+                    print(f"Error in sending message to chat {chat.id}: {e}")
+        await asyncio.sleep(sleeptimet)
+@l313l.ar_cmd(pattern="نشر_كروبات")
+async def Hussein(event):
+    await event.delete()
+    seconds = "".join(event.text.split(maxsplit=1)[1:]).split(" ", 2)
+    message =  await event.get_reply_message()
+    try:
+        sleeptimet = int(seconds[0])
+    except Exception:
+        return await edit_delete(
+            event, "⌔∮ يجب استخدام كتابة صحيحة الرجاء التاكد من الامر اولا ⚠️"
+        )
+    l313l = event.client
+    global yaAli
+    yaAli = True
+    await aljoker_allnshr(l313l, sleeptimet, message)
+super_groups = ["super", "سوبر"]
+async def aljoker_supernshr(l313l, sleeptimet, message):
+    global yaAli
+    yaAli = True
+    aljoker_chats = await l313l.get_dialogs()
+    while yaAli:
+        for chat in aljoker_chats:
+            chat_title_lower = chat.title.lower()
+            if chat.is_group and any(keyword in chat_title_lower for keyword in super_groups):
+                try:
+                    if message.media:
+                        await l313l.send_file(chat.id, message.media, caption=message.text)
+                    else:
+                        await l313l.send_message(chat.id, message.text)
+                except Exception as e:
+                    print(f"Error in sending message to chat {chat.id}: {e}")
+        await asyncio.sleep(sleeptimet)
+@l313l.ar_cmd(pattern="سوبر")
+async def Hussein(event):
+    await event.delete()
+    seconds = "".join(event.text.split(maxsplit=1)[1:]).split(" ", 2)
+    message =  await event.get_reply_message()
+    try:
+        sleeptimet = int(seconds[0])
+    except Exception:
+        return await edit_delete(
+            event, "⌔∮ يجب استخدام كتابة صحيحة الرجاء التاكد من الامر اولا ⚠️"
+        )
+    l313l = event.client
+    global yaAli
+    yaAli = True
+    await aljoker_supernshr(l313l, sleeptimet, message)
+@l313l.ar_cmd(pattern="ايقاف (النشر|نشر)")
+async def stop_aljoker(event):
+    global yaAli
+    yaAli = False
+    await event.edit("**᯽︙ تم ايقاف النشر التلقائي بنجاح ✓** ")
+
+message_counts = {}
+active_chats = set()
+disabled_chats = set()
+@l313l.ar_cmd(pattern="النشر تعطيل")
+async def Hussein(event):
+    chat_id = event.chat_id
+    if event.is_group:
+        if chat_id not in active_chats:
+            active_chats.add(chat_id)
+            disabled_chats.discard(chat_id)
+            await event.edit("**᯽︙✓ تم تفعيل امر منع النشر التلقائي **")
+        else:
+            await event.edit("**᯽︙ امر منع النشر التلقائي مُفعل بالفعل**")
+
+@l313l.ar_cmd(pattern="النشر تفعيل")
+async def Hussein(event):
+    chat_id = event.chat_id
+    if event.is_group:
+        if chat_id in active_chats:
+            active_chats.discard(chat_id)
+            disabled_chats.add(chat_id)
+            await event.edit("**᯽︙✓ تم تعطيل امر منع النشر التلقائي **")
+        else:
+            await event.edit("**᯽︙ امر منع النشر التلقائي معطل بالفعل **")
+
+@l313l.on(events.NewMessage)
+async def Hussein(event):
+    chat_id = event.chat_id
+    if chat_id in disabled_chats:
+        return
+    user_id = event.sender_id
+    message_text = event.text
+    if user_id not in message_counts:
+        message_counts[user_id] = {'last_message': None, 'count': 0}
+    if message_counts[user_id]['last_message'] == message_text:
+        message_counts[user_id]['count'] += 1
+    else:
+        message_counts[user_id]['last_message'] = message_text
+        message_counts[user_id]['count'] = 1
+    if message_counts[user_id]['count'] >= 3:
+        await client.edit_permissions(chat_id, user_id, send_messages=False)
+        sender = await event.get_sender()
+        aljoker_entity = await l313l.get_entity(sender.id)
+        aljoker_profile = f"[{aljoker_entity.first_name}](tg://user?id={aljoker_entity.id})"
+        explanation_message = f"**᯽︙ تم تقييد {aljoker_profile} من إرسال الرسائل بسبب تفعيله نشر التلقائي**"
+        await event.reply(explanation_message)
+        del message_counts[user_id]
