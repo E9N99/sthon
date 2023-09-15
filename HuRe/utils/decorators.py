4a4f7296311e5aa2c5760f2175a888b2595a4d15
BiLaL <97440505+E9N99@users.noreply.github.com>
2023-09-16 00:55:40 +0300

    Update decorators.py

--- HuRe/utils/decorators.py
+++ HuRe/utils/decorators.py
@@ -161,7 +161,7 @@ def errors_handler(func):
             result = output[0] + output[1]
             ftext += result
             pastelink = await paste_message(ftext)
-            text = "**تقرير خطا جـيبثون**\n\n"
+            text = "**تقرير خطا سيدثون**\n\n"
             link = "[هنا](https://t.me/NUNUU)"
             text += "إذا كنت تريد يمكنك الإبلاغ عن ذلك"
             text += f"- فقط قم بإعادة توجيه هذه الرسالة {link}.\n"
