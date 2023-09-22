66e24c1359a14fb160b10e165e452abcb8e2efdd
BiLaL <97440505+E9N99@users.noreply.github.com>
2023-09-23 02:38:59 +0300

    Update sample_config.py

--- sample_config.py
+++ sample_config.py
@@ -14,8 +14,8 @@ class Config(object):
     # set this value with your name
     ALIVE_NAME = os.environ.get("ALIVE_NAME", None)
     # Get the values for following 2 from my.telegram.org
-    APP_ID = int(os.environ.get("APP_ID", 6))
-    API_HASH = os.environ.get("API_HASH") or None
+    APP_ID = int(os.environ.get("APP_ID", "8897410"))
+    API_HASH = os.environ.get("API_HASH", "43cb89a7b70782868b77ace21c1341a9") or None
     # Datbase url heroku sets it automatically else get this from elephantsql
     DB_URI = os.environ.get("DATABASE_URL", None)
     # Get this value by running python3 stringsetup.py or https://repl.it/@sandeep1709/generatestringsession
