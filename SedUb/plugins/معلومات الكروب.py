c6b0ff2969bec3b7e6c3f4bb3ffa061cd54e4184
BiLaL <97440505+E9N99@users.noreply.github.com>
2023-09-24 16:25:54 +0300

    Update معلومات الكروب.py

--- SedUb/plugins/معلومات الكروب.py
+++ SedUb/plugins/معلومات الكروب.py
@@ -18,7 +18,7 @@ from telethon.tl.types import (
 )
 from telethon.utils import get_input_location
 
-from HuRe import l313l
+from SedUb import l313l
 
 from ..core.logger import logging
 from ..core.managers import edit_delete, edit_or_reply
