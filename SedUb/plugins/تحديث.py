245935830c6b9a2e80fd20648760f869c45ae0d7
BiLaL <97440505+E9N99@users.noreply.github.com>
2023-09-24 15:41:20 +0300

    Update تحديث.py

--- SedUb/plugins/تحديث.py
+++ SedUb/plugins/تحديث.py
@@ -270,9 +270,9 @@ async def upstream(event):
         origin = repo.create_remote("upstream", off_repo)
         origin.fetch()
         force_update = True
-        repo.create_head("HuRe", origin.refs.HuRe)
-        repo.heads.HuRe.set_tracking_branch(origin.refs.HuRe)
-        repo.heads.HuRe.checkout(True)
+        repo.create_head("SedUb", origin.refs.SedUb)
+        repo.heads.SedUb.set_tracking_branch(origin.refs.SedUb)
+        repo.heads.SedUb.checkout(True)
     ac_br = repo.active_branch.name
     if ac_br != UPSTREAM_REPO_BRANCH:
         await event.edit(
@@ -345,7 +345,7 @@ async def Hussein(event):
         repo = Repo.init()
         origin = repo.create_remote("upstream", off_repo)
         origin.fetch()
-        repo.create_head("HuRe", origin.refs.master)
+        repo.create_head("SedUb", origin.refs.master)
         repo.heads.SedUb.set_tracking_branch(origin.refs.master)
         repo.heads.SedUb.checkout(True)
     with contextlib.suppress(BaseException):
@@ -390,7 +390,7 @@ async def reda(event):
             origin = repo.create_remote("upstream", off_repo)
             origin.fetch()
             force_update = True
-            repo.create_head("HuRe", origin.refs.SedUb)
+            repo.create_head("SedUb", origin.refs.SedUb)
             repo.heads.SedUb.set_tracking_branch(origin.refs.SedUb)
             repo.heads.SedUb.checkout(True)
         ac_br = repo.active_branch.name
