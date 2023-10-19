1bebacce26e0697203c2be5ce3e8044ea12ff904
BiLaL <97440505+E9N99@users.noreply.github.com>
2023-10-19 23:54:52 +0300

    Update الوقتيةةةةة.py

--- SedUb/plugins/الوقتيةةةةة.py
+++ SedUb/plugins/الوقتيةةةةة.py
@@ -12,7 +12,7 @@ import shutil
 import time
 from datetime import datetime
 from telethon import events
-from ALJoker import get_string
+from BiLaL import get_string
 from telethon.errors import ChatAdminRequiredError
 from PIL import Image, ImageDraw, ImageFont
 from pySmartDL import SmartDL
