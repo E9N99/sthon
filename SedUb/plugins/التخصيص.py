a4880f4811f0fc5c702489cc773812f889760af4
BiLaL <97440505+E9N99@users.noreply.github.com>
2023-10-19 23:56:41 +0300

    Update التخصيص.py

--- SedUb/plugins/التخصيص.py
+++ SedUb/plugins/التخصيص.py
@@ -64,15 +64,15 @@ async def custom_HuRe(event):
     if input_str == "امر النشر" or input_str == "امر نشر":
         addgvar("MUKRR_ET", text)
         var = "MUKRR_ET"
-        if input_str == "زخرفة الارقام" or input_str == "زخرفه الارقام":
-         addgvar("JP_FN", text)
-         var = "JP_FN"
-     if input_str == "البايو" or input_str == "بايو":
-         addgvar("DEFAULT_BIO", text)
-         var = "DEFAULT_BIO"
-     if input_str == "رمز الاسم" or input_str == "علامة الاسم":
-         addgvar("TIME_JEP", text)
-         var = "TIME_JEP"
+    if input_str == "زخرفة الارقام" or input_str == "زخرفه الارقام":
+        addgvar("JP_FN", text)
+        var = "JP_FN"
+    if input_str == "البايو" or input_str == "بايو":
+        addgvar("DEFAULT_BIO", text)
+        var = "DEFAULT_BIO"
+    if input_str == "رمز الاسم" or input_str == "علامة الاسم":
+        addgvar("TIME_JEP", text)
+        var = "TIME_JEP"
     if input_str == "كليشة الفحص" or input_str == "كليشه الفحص" or input_str == "كليشه فحص" or input_str == "كليشه فحص":
         addgvar("ALIVE_TEMPLATE", text)
         var = "ALIVE_TEMPLATE"
@@ -197,24 +197,24 @@ async def custom_HuRe(event):
                 event, "**⎙ :: عزيزي المستخدم انت لم تقوم باضافه هذا الفار اصلا**"
             )
         delgvar("ALIVE_TEXT")
-        if input_str == "زخرفة الاسم" or input_str == "زخرفه الاسم":
-         if gvarstatus("JP_FN") is None:
-             return await edit_delete(
-                 event, "**⎙ :: عزيزي المستخدم انت لم تقوم باضافه هذا الفار اصلا**"
-             )
-         delgvar("JP_FN")
-     if input_str == "بايو" or input_str == "البايو":
-         if gvarstatus("DEFAULT_BIO") is None:
-             return await edit_delete(
-                 event, "**⎙ :: عزيزي المستخدم انت لم تقوم باضافه هذا الفار اصلا**"
-             )
-         delgvar("DEFAULT_BIO")
-     if input_str == "رمز الاسم":
-         if gvarstatus("TIME_JEP") is None:
-             return await edit_delete(
-                 event, "**⎙ :: عزيزي المستخدم انت لم تقوم باضافه هذا الفار اصلا**"
-             )
-         delgvar("TIME_JEP")
+    if input_str == "زخرفة الاسم" or input_str == "زخرفه الاسم":
+        if gvarstatus("JP_FN") is None:
+            return await edit_delete(
+                event, "**⎙ :: عزيزي المستخدم انت لم تقوم باضافه هذا الفار اصلا**"
+            )
+        delgvar("JP_FN")
+    if input_str == "بايو" or input_str == "البايو":
+        if gvarstatus("DEFAULT_BIO") is None:
+            return await edit_delete(
+                event, "**⎙ :: عزيزي المستخدم انت لم تقوم باضافه هذا الفار اصلا**"
+            )
+        delgvar("DEFAULT_BIO")
+    if input_str == "رمز الاسم":
+        if gvarstatus("TIME_JEP") is None:
+            return await edit_delete(
+                event, "**⎙ :: عزيزي المستخدم انت لم تقوم باضافه هذا الفار اصلا**"
+            )
+        delgvar("TIME_JEP")
     if input_str == "عدد التحذيرات":
         if gvarstatus("MAX_FLOOD_IN_PMS") is None:
             return await edit_delete(
