cec7a7750b122795a7d79031369364d7b2ac1f8d
BiLaL <97440505+E9N99@users.noreply.github.com>
2023-10-20 21:34:13 +0300

    Update رسالة.py

--- SedUb/plugins/رسالة.py
+++ SedUb/plugins/رسالة.py
@@ -24,7 +24,7 @@ LOGS = logging.getLogger(__name__)
             "{tr}رسالة <username/chatid/chatusername> او بالرد على الرسالة",
             "{tr}رسالة <username/userid/chatid/chatusername> <text>",
         ],
-        "examples": "{tr}رسالة @jepthon حي الله  ان",
+        "examples": "{tr}رسالة @veevvw حي الله  ان",
     },
 )
 async def catbroadcast_add(event):
