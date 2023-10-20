2dbc04d7e54183d5b3ae56e033ee52f7d00cb703
BiLaL <97440505+E9N99@users.noreply.github.com>
2023-10-20 15:56:52 +0300

    Update botmanagers.py

--- SedUb/assistant/botmanagers.py
+++ SedUb/assistant/botmanagers.py
@@ -3,7 +3,7 @@ from math import floor
 
 from telethon.utils import get_display_name
 
-from JoKeRUB import l313l
+from SedUb import l313l
 
 from ..Config import Config
 from ..core.logger import logging
