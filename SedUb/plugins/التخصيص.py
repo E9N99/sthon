6075b2347254282dbdaed3d98428ec16202ff843
BiLaL <97440505+E9N99@users.noreply.github.com>
2023-10-20 20:02:31 +0300

    Update التخصيص.py

--- SedUb/plugins/التخصيص.py
+++ SedUb/plugins/التخصيص.py
@@ -1,8 +1,8 @@
 from urlextract import URLExtract
 import re
 import requests
-from JoKeRUB import l313l
-from JoKeRUB.core.logger import logging
+from SedUb import l313l
+from SedUb.core.logger import logging
 from ..Config import Config
 from ..core.managers import edit_delete, edit_or_reply
 from ..sql_helper.globals import addgvar, delgvar, gvarstatus
