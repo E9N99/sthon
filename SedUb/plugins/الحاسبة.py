703a33928e1e7a5ea706180ae5442e2b820064bb
BiLaL <97440505+E9N99@users.noreply.github.com>
2023-10-20 20:16:37 +0300

    Update الحاسبة.py

--- SedUb/plugins/الحاسبة.py
+++ SedUb/plugins/الحاسبة.py
@@ -3,9 +3,9 @@ import re
 from telethon import Button
 from telethon.events import CallbackQuery, InlineQuery
 
-from JoKeRUB import CMD_HELP, l313l
+from SedUb import CMD_HELP, l313l
 
-# 𝗧𝗲𝗹𝗲𝗚𝗿𝗮𝗠 : @jepthon  ~ @lMl10l
+# 𝗧𝗲𝗹𝗲𝗚𝗿𝗮𝗠 : @veevvw  ~ @nunuu
 from ..core.decorators import check_owner
 
 CALC = {}
@@ -42,7 +42,7 @@ lst.append([Button.inline("=", data="calc=")])
 async def icalc(e):
     if e.client._bot:
         return await e.reply(
-            "**الحـاسبة العـلمية لسـورس الجوكر\n @jepthon**", buttons=lst
+            "**الحـاسبة العـلمية لسـورس سيدثرن\n @veevvw**", buttons=lst
         )
     results = await e.client.inline_query(Config.TG_BOT_USERNAME, "calc")
     await results[0].click(e.chat_id, silent=True, hide_via=True)
@@ -59,7 +59,7 @@ async def inlinecalc(event):
     ) and string == "calc":
         event.builder
         calc = event.builder.article(
-            "Calc", text="**الحـاسبة العـلمية لسـورس الجوكر\n @jepthon**", buttons=lst
+            "Calc", text="**الحـاسبة العـلمية لسـورس سيدثوو\n @veevvw**", buttons=lst
         )
         await event.answer([calc])
 
@@ -75,7 +75,7 @@ async def _(e):  # sourcery no-metrics
         if CALC.get(user):
             CALC.pop(user)
         await e.edit(
-            "**الحـاسبة العـلمية لسـورس الجوكر\n @jepthon**",
+            "**الحـاسبة العـلمية لسـورس سيدثون\n @veevvw**",
             buttons=[Button.inline("افتح مره اخرى", data="recalc")],
         )
     elif x == "C":
@@ -130,7 +130,7 @@ async def _(e):  # sourcery no-metrics
         await e.answer(str(x))
 
 
-# 𝗧𝗲𝗹𝗲𝗚𝗿𝗮𝗠 : @jepthon  ~ @lMl10l
+# 𝗧𝗲𝗹𝗲𝗚𝗿𝗮𝗠 : @veevve  ~ @nunuu
 @l313l.tgbot.on(CallbackQuery(data=re.compile(b"recalc")))
 @check_owner
 async def _(e):
@@ -159,7 +159,7 @@ async def _(e):
     tultd = [Button.inline(f"{x}", data=f"calc{x}") for x in m]
     lst = list(zip(tultd[::4], tultd[1::4], tultd[2::4], tultd[3::4]))
     lst.append([Button.inline("=", data="calc=")])
-    await e.edit("**الحـاسبة العـلمية لسـورس الجوكر\n @jepthon**", buttons=lst)
+    await e.edit("**الحـاسبة العـلمية لسـورس سيدثون  \n @veevve**", buttons=lst)
 
 CMD_HELP.update(
     {"الحسابة": ".حاسبة" "\n فقط اكتب الامر لعرض حاسبة علميه تحتاج الى تفعيل وضع الانلاين اولا\n\n"}
