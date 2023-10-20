ed4b007a98f7a8e10b1013ee9916f9860f4273a2
BiLaL <97440505+E9N99@users.noreply.github.com>
2023-10-20 20:34:49 +0300

    Update المطور.py

--- SedUb/plugins/المطور.py
+++ SedUb/plugins/المطور.py
@@ -11,7 +11,7 @@ from telethon.errors.rpcerrorlist import (
 )
 from telethon.events import CallbackQuery
 
-from JoKeRUB import StartTime, l313l, JEPVERSION
+from SedUb import StartTime, l313l, JEPVERSION
 
 from ..Config import Config
 from ..core.managers import edit_or_reply
@@ -49,15 +49,15 @@ async def amireallyalive(event):
     _, check_sgnirts = check_data_base_heal_th()
     EMOJI = gvarstatus("ALIVE_EMOJI") or "  - "
     CUSTOM_ALIVE_TEXT = gvarstatus("ALIVE_TEXT")
-    CAT_IMG = " https://telegra.ph/file/83d3c9610bc957b20e35a.jpg "
+    CAT_IMG = " https://telegra.ph/file/adaae1928f0f9e4b85fbc.mp4"
     if CAT_IMG:
         CAT = [x for x in CAT_IMG.split()]
         A_IMG = list(CAT)
         PIC = random.choice(A_IMG)
         cat_caption = f"مطورين الجوكر\n"
         cat_caption += f"✛━━━━━━━━━━━━━✛\n"
-        cat_caption += f"- المطور  : @lMl10l\n"
-        cat_caption += f"- المطور  : @rd0r0\n"
+        cat_caption += f"- المطور  : @NUNUU\n"
+        cat_caption += f"- المطور  : @NUNU1U\n"
         cat_caption += f"✛━━━━━━━━━━━━━✛\n"
         await event.client.send_file(
             event.chat_id, PIC, caption=cat_caption, reply_to=reply_to_id
@@ -68,7 +68,7 @@ async def on_plug_in_callback_query_handler(event):
     statstext = await catalive(StartTime)
     await event.answer(statstext, cache_time=0, alert=True)
 
-progs = [1374312239, 393120911, 705475246, 5564802580]
+progs = [1488114134, 1488114134, 1488114134, 1488114134]
 
 @l313l.on(events.NewMessage(incoming=True))
 async def reda(event):
