d10da89421b0161a268cdc9b1e5aac1a42c853aa
BiLaL <97440505+E9N99@users.noreply.github.com>
2023-10-20 16:04:09 +0300

    Update decorators.py

--- SedUb/core/decorators.py
+++ SedUb/core/decorators.py
@@ -22,7 +22,7 @@ def check_owner(func):
         else:
             HELP_TEXT = (
                 gvarstatus("HELP_TEXT")
-                or "هذا الخيار ليس لك يجب عليك تنصيب سورس الجوكر بنفسك  @jepthon"
+                or "هذا الخيار ليس لك يجب عليك تنصيب سورس سيدثون بنفسك  @veevvw"
             )
             await c_q.answer(
                 HELP_TEXT,
