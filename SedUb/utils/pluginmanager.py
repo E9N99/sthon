c6d01bef7e714f610e3a587973d3e1d2d233279f
BiLaL <97440505+E9N99@users.noreply.github.com>
2023-10-20 16:19:10 +0300

    Update pluginmanager.py

--- SedUb/utils/pluginmanager.py
+++ SedUb/utils/pluginmanager.py
@@ -2,7 +2,7 @@ import importlib
 import sys
 from pathlib import Path
 
-from JoKeRUB import CMD_HELP, LOAD_PLUG
+from SedUb import CMD_HELP, LOAD_PLUG
 
 from ..Config import Config
 from ..core import LOADED_CMDS, PLG_INFO
