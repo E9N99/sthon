b5077e2a9876a697cf73bd3fd25dca1b78a93839
BiLaL <97440505+E9N99@users.noreply.github.com>
2023-10-20 20:42:28 +0300

    Update امونغ اس.py

--- SedUb/plugins/امونغ اس.py
+++ SedUb/plugins/امونغ اس.py
@@ -9,7 +9,7 @@ from PIL import Image, ImageDraw, ImageFont
 from requests import get
 from telethon.utils import get_display_name
 
-from JoKeRUB import l313l
+from SedUb import l313l
 
 from ..Config import Config
 from ..core.managers import edit_or_reply
@@ -18,14 +18,14 @@ from . import ALIVE_NAME
 
 plugin_category = "extra"
 
-#كـتابة وتعـديل  @lMl10l
-# JoKeRUB ™
+#كـتابة وتعـديل  @nunuu
+# SedUb ™
 async def amongus_gen(text: str, clr: int) -> str:
-    url = "https://github.com/JoKeRUB-AR/l313l-Resources/raw/master/Resources/Amongus/"
+    url = "https://github.com/SedUb-AR/l313l-Resources/raw/master/Resources/Amongus/"
     font = ImageFont.truetype(
         BytesIO(
             get(
-                "https://github.com/JoKeRUB-AR/l313l-Resources/raw/master/Resources/fonts/bold.ttf"
+                "https://github.com/SedUb-AR/l313l-Resources/raw/master/Resources/fonts/bold.ttf"
             ).content
         ),
         60,
@@ -54,10 +54,10 @@ async def amongus_gen(text: str, clr: int) -> str:
 
 async def get_imposter_img(text: str) -> str:
     background = get(
-        f"https://github.com/JoKeRUB-AR/l313l-Resources/raw/master/Resources/imposter/impostor{randint(1,22)}.png"
+        f"https://github.com/SedUb-AR/l313l-Resources/raw/master/Resources/imposter/impostor{randint(1,22)}.png"
     ).content
     font = get(
-        "https://github.com/JoKeRUB-AR/l313l-Resources/raw/master/Resources/fonts/roboto_regular.ttf"
+        "https://github.com/SedUb-AR/l313l-Resources/raw/master/Resources/fonts/roboto_regular.ttf"
     ).content
     font = BytesIO(font)
     font = ImageFont.truetype(font, 30)
