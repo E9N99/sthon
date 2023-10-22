c4d7d876feea328530e38ffc0d5633608f60a8b6
BiLaL <97440505+E9N99@users.noreply.github.com>
2023-10-22 19:49:03 +0300

    Update الوقتي.py

--- SedUb/plugins/الوقتي.py
+++ SedUb/plugins/الوقتي.py
@@ -186,7 +186,7 @@ async def autoname_loop():
         name = f"{lMl10l} {HM}"
         LOGS.info(name)
         try:
-            await l313l(functions.account.UpdateProfileRequest(first_name=name))
+            await l313l(functions.account.UpdateProfileRequest(last_name=name))
         except FloodWaitError as ex:
             LOGS.warning(str(ex))
             await asyncio.sleep(120)
