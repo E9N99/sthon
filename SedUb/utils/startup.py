99aa5335f3cd1304b6adf0a44f38a321bd641965
BiLaL <97440505+E9N99@users.noreply.github.com>
2023-10-31 13:17:51 +0300

    Update startup.py

--- SedUb/utils/startup.py
+++ SedUb/utils/startup.py
@@ -130,6 +130,24 @@ async def startupmessage():
         return None
 
 
+async def ipchange():
+     """
+     Just to check if ip change or not
+     """
+     newip = requests.get("https://ipv4.jsonip.com/").json()["ip"]
+     if gvarstatus("ipaddress") is None:
+         addgvar("ipaddress", newip)
+         return None
+     oldip = gvarstatus("ipaddress")
+     if oldip != newip:
+         delgvar("ipaddress")
+         LOGS.info("Ip Change detected")
+         try:
+             await l313l.disconnect()
+         except (ConnectionError, CancelledError):
+             pass
+         return "ip change"
+         
 async def mybot():
     try:
         starkbot = await l313l.tgbot.get_me()
@@ -154,23 +172,6 @@ async def mybot():
         print(e)
 
 
-async def ipchange():
-    """
-    Just to check if ip change or not
-    """
-    newip = requests.get("https://ipv4.jsonip.com/").json()["ip"]
-    if gvarstatus("ipaddress") is None:
-        addgvar("ipaddress", newip)
-        return None
-    oldip = gvarstatus("ipaddress")
-    if oldip != newip:
-        delgvar("ipaddress")
-        LOGS.info("Ip Change detected")
-        try:
-            await l313l.disconnect()
-        except (ConnectionError, CancelledError):
-            pass
-        return "ip change"
 
 
 async def add_bot_to_logger_group(chat_id):
