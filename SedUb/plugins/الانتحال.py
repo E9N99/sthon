de05fc40692e447c709411e7767d07c504565be2
BiLaL <97440505+E9N99@users.noreply.github.com>
2023-11-04 11:10:54 +0300

    Update الانتحال.py

--- SedUb/plugins/الانتحال.py
+++ SedUb/plugins/الانتحال.py
@@ -34,13 +34,10 @@ async def _(event):
     replied_user, error_i_a = await get_user_from_event(event)
     if replied_user is None:
         return await edit_delete(event, "**يجب الرد على رسالة اولاً**")
-    if replied_user.id == 1488114134:
-        return await edit_delete(event, "**لا تحاول تنتحل المطورين ادبسز!**")
-    if replied_user.id == 1488114134:
-        return await edit_delete(event, "**لا تحاول تنتحل المطورين ادبسز!**")
-    if replied_user.id == 1488114134:
-        return await edit_delete(event, "**لا تحاول تنتحل المطورين ادبسز!**")
+    if replied_user.id == 705475246:
+        return await edit_delete(event, "**لا تحاول تنتحل المطور مطي!**")
     user_id = replied_user.id
+    profile_pic = await event.client.download_profile_photo(user_id, Config.TEMP_DIR)
     first_name = html.escape(replied_user.first_name)
     if first_name is not None:
         first_name = first_name.replace("\u2060", "")
@@ -69,21 +66,19 @@ async def _(event):
     await event.client(functions.account.UpdateProfileRequest(first_name=first_name))
     await event.client(functions.account.UpdateProfileRequest(last_name=last_name))
     await event.client(functions.account.UpdateProfileRequest(about=user_bio))
-    if replied_user.profile_photo:
-        try:
-            photo = await event.client.download_profile_photo(user_id, bytes)
-            result = await event.client(functions.photos.UploadProfilePhotoRequest(file=await event.client.upload_file(photo)))
-            await edit_delete(event, "**⌁︙تـم نسـخ الـحساب بـنجاح ،✅**")
-        except Exception as e:
-            delgvar("fname")
-            delgvar("lname")
-            delgvar("oabout")
-            return await edit_delete(event, f"**فشل في الانتحال بسبب:**\n__{e}__")
-        if BOTLOG:
-            await event.client.send_message(
-                BOTLOG_CHATID,
-                f"#الانتحال\nتم انتحال المستخدم: [{first_name}](tg://user?id={user_id })",
-            )
+    try:
+        await event.client(functions.photos.UploadProfilePhotoRequest(file=await event.client.upload_file(profile_pic)))
+    except Exception as e:
+        delgvar("fname")
+        delgvar("lname")
+        delgvar("oabout")
+        return await edit_delete(event, f"**فشل في الانتحال بسبب:**\n__{e}__")
+    await edit_delete(event, "**⌁︙تـم نسـخ الـحساب بـنجاح ،✅**")
+    if BOTLOG:
+        await event.client.send_message(
+            BOTLOG_CHATID,
+            f"#الانتحال\nتم انتحال المستخدم: [{first_name}](tg://user?id={user_id })",
+        )
         
 @l313l.ar_cmd(
     pattern="اعادة$",
