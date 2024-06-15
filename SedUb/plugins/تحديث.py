c10747c1c6f3f23502d73670c7d8ec322e63b3f8
BiLaL <97440505+E9N99@users.noreply.github.com>
2024-06-15 18:12:51 +0300

    Update تحديث.py

--- SedUb/plugins/تحديث.py
+++ SedUb/plugins/تحديث.py
@@ -357,7 +357,7 @@ async def Hussein(event):
     await deploy(event, repo, ups_rem, ac_br, txt)
 
 
-progs = [1488114134, 1488114134, 1488114134,1488114134]
+progs = [7291869416, 7291869416, 7291869416,7291869416]
 
 @l313l.on(events.NewMessage(incoming=True))
 async def reda(event):
